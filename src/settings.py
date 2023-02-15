0f75160bf2aa3dfc6d80997fb09c3be56eb30f34
XaN0037 <xannodirbek@gmail.com>
2023-02-15 16:37:47 +0500

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-#
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
@@ -157,3 +157,7 @@ MEDIA_URL = "media/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+PER_PAGE = 5
