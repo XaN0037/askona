da613d328932738fc6db2b0b7a8c9c91dd2a98ae
XaN0037 <xannodirbek@gmail.com>
2023-02-16 12:17:32 +0500

    Update models.py

--- api/models.py
+++ api/models.py
@@ -42,7 +42,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     def __str__(self):
-        return  self.name, self.mobile
+        return self.mobile
 
 
 
